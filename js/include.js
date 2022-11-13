e844f17452aec04dfcfdb37c8359914214f82f3b
ju <vial222@gmail.com>
2022-11-14 01:23:13 +0900

    first commit

---
+++ js/include.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $("#header").load("./include/header.html"); 
+    });
+$(function(){
+    $("#footer").load("./include/footer.html"); 
+    });
+
+$(function(){
+    $("#pagenate").load("./include/pagenate.html"); 
+    });
+
+$(function(){
+    $("#calendar").load("./include/calendar.html"); 
+    });
